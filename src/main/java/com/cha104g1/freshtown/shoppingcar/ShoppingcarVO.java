e2bacb2226d6b988d8470d7b6f7739c1c6f38076
jason861014 <jason30615@gmail.com>
2023-12-16 11:03:40 +0800

    加入 Shoppingcar model

---
+++ src/main/java/com/cha104g1/freshtown/shoppingcar/ShoppingcarVO.java
@@ -0,0 +1,31 @@
+package com.cha104g1.freshtown.shoppingcar;
+
+public class ShoppingcarVO {
+    private  Integer num;
+    private String Acc;
+    private String password;
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getAcc() {
+        return Acc;
+    }
+
+    public void setAcc(String acc) {
+        Acc = acc;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
