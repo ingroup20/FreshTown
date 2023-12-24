e122f68db02234a632e1677ebbff6a99fa87a0b0
Y.J <azrael0502@gmail.com>
2023-12-24 23:02:59 +0800

    平台員工和店家員工的VO和DAO介面

---
+++ src/main/java/com/cha104g1/freshtown/store_emp/model/StoreEmpDAO_Interface.java
@@ -0,0 +1,13 @@
+package com.cha104g1.freshtown.store_emp.model;
+
+import java.util.*;
+
+public interface StoreEmpDAO_Interface {
+	
+	public void insert(StoreEmpVO storeEmpVO);
+    public void update(StoreEmpVO storeEmpVO);
+    public void delete(Integer sEmpId);
+    public StoreEmpVO findByPrimaryKey(Integer sEmpId);
+    public List<StoreEmpVO> getAll();
+
+}
