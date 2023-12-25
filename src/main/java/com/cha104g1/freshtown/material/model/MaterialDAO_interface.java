afe0d0d54af3c276bc2242e3333a462f63bd9f7e
ingroup20 <148344044+ingroup20@users.noreply.github.com>
2023-12-25 18:26:44 +0800

    Revert "刪除檔案"

--- src/main/java/com/cha104g1/freshtown/material/model/MaterialDAO_interface.java
+++ src/main/java/com/cha104g1/freshtown/material/model/MaterialDAO_interface.java
@@ -2,9 +2,9 @@ package com.cha104g1.freshtown.material.model;
 
 import java.util.*;
 
-public interface MaterialDAOIntf {
+public interface MaterialDAO_interface {
     public void insert(MaterialVO materialVO);
     public void update(MaterialVO materialVO);
-    public MaterialVO findByPrimaryKey(Integer itemNumber);
+    public MaterialVO findByPrimaryKey(Integer itemnumber);
     public List<MaterialVO> getAll();
 }
